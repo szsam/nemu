22e0e69d8bec4d7ecf39205a8356bfd4fbb9131e
151220091-Shen MingJie <151220091@smail.nju.edu.cn>
2018-04-06 19:35:31 +0800

    Implement memory mapped IO

--- src/memory/memory.c
+++ src/memory/memory.c
@@ -1,4 +1,5 @@
 #include "nemu.h"
+#include "device/mmio.h"
 
 #define PMEM_SIZE (128 * 1024 * 1024)
 
@@ -12,11 +13,19 @@ uint8_t pmem[PMEM_SIZE];
 /* Memory accessing interfaces */
 
 uint32_t paddr_read(paddr_t addr, int len) {
-  return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
+  int no = is_mmio(addr);
+  if (no == -1)
+	return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
+  else
+	return mmio_read(addr, len, no);
 }
 
 void paddr_write(paddr_t addr, int len, uint32_t data) {
-  memcpy(guest_to_host(addr), &data, len);
+  int no = is_mmio(addr);
+  if (no == -1)
+	memcpy(guest_to_host(addr), &data, len);
+  else
+	mmio_write(addr, len, data, no);
 }
 
 uint32_t vaddr_read(vaddr_t addr, int len) {
