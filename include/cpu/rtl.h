708fd58808e205418aa5545f0265f385c1142552
MJ Shen <smj_1997@126.com>
2018-05-31 17:52:44 +0800

    Implement RTL pseudo instructions using RTL basic instructions

--- include/cpu/rtl.h
+++ include/cpu/rtl.h
@@ -149,7 +149,7 @@ static inline void rtl_sr(int r, int width, const rtlreg_t* src1) {
 
 static inline void rtl_mv(rtlreg_t* dest, const rtlreg_t *src1) {
   // dest <- src1
-  *dest = *src1;
+  rtl_add(dest, src1, &tzero);
 }
 
 void cc_gen_ZF(rtlreg_t *);
@@ -173,33 +173,37 @@ make_rtl_setget_eflags(SF)
 
 static inline void rtl_not(rtlreg_t* dest) {
   // dest <- ~dest
-  *dest = ~*dest;
+  rtl_xori(dest, dest, 0xffffffff);
 }
 
 static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- signext(src1[(width * 8 - 1) .. 0])
   int n = 32 - width * 8;
-  *dest = (int32_t)(*src1 << n) >> n;
+  // *dest = (int32_t)(*src1 << n) >> n;
+  if (n != 0) {
+	rtl_shli(dest, src1, n);
+  	rtl_sari(dest, dest, n);
+  }
 }
 
 static inline void rtl_push(const rtlreg_t* src1) {
   // esp <- esp - 4
   // M[esp] <- src1
   
-	cpu.esp -= 4;
-	vaddr_write(cpu.esp, 4, *src1);
+  rtl_subi(&cpu.esp, &cpu.esp, 4);
+  rtl_sm(&cpu.esp, 4, src1);
 }
 
 static inline void rtl_pop(rtlreg_t* dest) {
   // dest <- M[esp]
   // esp <- esp + 4
-  *dest = vaddr_read(cpu.esp, 4);
-  cpu.esp += 4;
+  rtl_lm(dest, &cpu.esp, 4);
+  rtl_addi(&cpu.esp, &cpu.esp, 4);
 }
 
 static inline void rtl_eq0(rtlreg_t* dest, const rtlreg_t* src1) {
   // dest <- (src1 == 0 ? 1 : 0)
-  TODO();
+  rtl_setrelop(RELOP_EQ, dest, src1, &tzero);
 }
 
 static inline void rtl_eqi(rtlreg_t* dest, const rtlreg_t* src1, int imm) {
@@ -214,18 +218,26 @@ static inline void rtl_neq0(rtlreg_t* dest, const rtlreg_t* src1) {
 
 static inline void rtl_msb(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- src1[width * 8 - 1]
-  *dest = (*src1 >> (width * 8 - 1)) & 0x1;
+  // *dest = (*src1 >> (width * 8 - 1)) & 0x1;
+  int n = width * 8 - 1;
+  rtl_shri(dest, src1, n);
+  rtl_andi(dest, dest, 0x1);
 }
 
 static inline void rtl_update_ZF(const rtlreg_t* result, int width) {
   // eflags.ZF <- is_zero(result[width * 8 - 1 .. 0])
   uint32_t mask = 0xffffffffu >> (32 - 8 * width);
-  cpu.eflags.ZF = (*result & mask) == 0;
+  // cpu.eflags.ZF = (*result & mask) == 0;
+  rtl_andi(&at, result, mask);
+  rtl_eq0(&at, &at);
+  rtl_set_ZF(&at);
 }
 
 static inline void rtl_update_SF(const rtlreg_t* result, int width) {
   // eflags.SF <- is_sign(result[width * 8 - 1 .. 0])
-  cpu.eflags.SF = (*result >> (8 * width - 1)) & 0x1;
+  // cpu.eflags.SF = (*result >> (8 * width - 1)) & 0x1;
+  rtl_msb(&at, result, width);
+  rtl_set_SF(&at);
 }
 
 static inline void rtl_update_ZFSF(const rtlreg_t* result, int width) {
--- src/cpu/exec/cc.c
+++ src/cpu/exec/cc.c
@@ -14,13 +14,27 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
   // TODO: Query EFLAGS to determine whether the condition code is satisfied.
   // dest <- ( cc is satisfied ? 1 : 0)
   switch (subcode & 0xe) {
-    case CC_O: *dest = cpu.eflags.OF; break;
-    case CC_B: *dest = cpu.eflags.CF; break;
-    case CC_E: *dest = cpu.eflags.ZF; break;
-    case CC_BE: *dest = (cpu.eflags.ZF || cpu.eflags.CF); break;
-    case CC_S: *dest = cpu.eflags.SF; break;
-    case CC_L: *dest = (cpu.eflags.SF != cpu.eflags.OF); break;
-    case CC_LE: *dest = (cpu.eflags.ZF || (cpu.eflags.SF != cpu.eflags.OF)); break;
+    case CC_O: rtl_get_OF(dest); break;
+    case CC_B: rtl_get_CF(dest); break;
+    case CC_E: rtl_get_ZF(dest); break;
+    case CC_BE: // *dest = (cpu.eflags.ZF || cpu.eflags.CF); break;
+		rtl_get_ZF(&t0);
+		rtl_get_CF(&t1);
+		rtl_or(dest, &t0, &t1);
+		break;
+    case CC_S: rtl_get_SF(dest);  break;
+    case CC_L: // *dest = (cpu.eflags.SF != cpu.eflags.OF); break;
+		rtl_get_SF(&t0);
+		rtl_get_OF(&t1);
+		rtl_xor(dest, &t0, &t1);
+		break;
+    case CC_LE: // *dest = (cpu.eflags.ZF || (cpu.eflags.SF != cpu.eflags.OF)); break;
+		rtl_get_SF(&t0);
+		rtl_get_OF(&t1);
+		rtl_xor(&t0, &t0, &t1);
+		rtl_get_ZF(&t1); 
+		rtl_or(dest, &t0, &t1);
+		break;
     default: panic("should not reach here");
     case CC_P: panic("n86 does not have PF");
   }
