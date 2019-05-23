0e103d770014f48ce1c727dc34164c30b20d8805
MJ Shen <smj_1997@126.com>
2019-05-23 16:49:43 +0800

    gen rtl insn for calculating ZF and SF

--- src/cpu/exec/logic.c
+++ src/cpu/exec/logic.c
@@ -60,6 +60,7 @@ make_EHelper(or) {
 make_EHelper(sar) {
   rtl_sar(id_dest->val, id_dest->val, id_src->val);
   operand_write(id_dest, id_dest->val);
+  gen_rtl_update_cc_logic2(id_dest->width, id_dest->val);
 //  rtl_update_ZFSF(id_dest->val, id_dest->width);
   
   // unnecessary to update CF and OF in NEMU
@@ -70,6 +71,7 @@ make_EHelper(sar) {
 make_EHelper(shl) {
   rtl_shl(id_dest->val, id_dest->val, id_src->val);
   operand_write(id_dest, id_dest->val);
+  gen_rtl_update_cc_logic2(id_dest->width, id_dest->val);
 //  rtl_update_ZFSF(id_dest->val, id_dest->width);
   // unnecessary to update CF and OF in NEMU
 
@@ -79,6 +81,7 @@ make_EHelper(shl) {
 make_EHelper(shr) {
   rtl_shr(id_dest->val, id_dest->val, id_src->val);
   operand_write(id_dest, id_dest->val);
+  gen_rtl_update_cc_logic2(id_dest->width, id_dest->val);
 //  rtl_update_ZFSF(id_dest->val, id_dest->width);
   // unnecessary to update CF and OF in NEMU
 
